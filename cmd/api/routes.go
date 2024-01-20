5a7abdcb4cfb3bd21a71ddcbb75212e6f5709f20
Makism-Gr <maksim.g.grishchenko@gmail.com>
2024-01-20 20:42:23 +0300

    chore:add new routes

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,10 +1,16 @@
 package main
 
-import "github.com/julienschmidt/httprouter"
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
 
 func (app *application) routes() *httprouter.Router {
 	//Initialize a new router instance
 	router := httprouter.New()
+
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/healthcheck", app.createMovieHandler)
 
 	return router
 }
