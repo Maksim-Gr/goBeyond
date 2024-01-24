57675d334423c09c5d2ca62cb69879065e84cc73
Makism-Gr <maksim.g.grishchenko@gmail.com>
2024-01-24 20:27:33 +0300

    chore: add errorResponse && logError methods

---
+++ cmd/api/errors.go
@@ -0,0 +1,17 @@
+package main
+
+import "net/http"
+
+func (app *application) logError(r *http.Request, err error) {
+	app.logger.Println(err)
+}
+
+func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message interface{}) {
+	env := envelope{"error": message}
+
+	err := app.WriteJson(w, status, env, nil)
+	if err != nil {
+		app.logError(r, err)
+		w.WriteHeader(500)
+	}
+}
