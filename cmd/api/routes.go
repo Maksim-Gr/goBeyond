e8ed297c7a22af1ac4ffd66a956d9a55e810d12c
Makism-Gr <maksim.g.grishchenko@gmail.com>
2024-01-25 14:10:25 +0300

    update: update router for not allowed and not found errors

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,8 +9,13 @@ func (app *application) routes() *httprouter.Router {
 	//Initialize a new router instance
 	router := httprouter.New()
 
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
-	router.HandlerFunc(http.MethodPost, "/v1/healthcheck", app.createMovieHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/movie", app.createMovieHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 
 	return router
 }
