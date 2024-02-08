79fcbf4057110874353ad6a4622d81beb6d8c9c6
Makism-Gr <maksim.g.grishchenko@gmail.com>
2024-02-08 20:22:55 +0300

    update: update movies get method

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"github.com/Maksim-Gr/goBeyond/internal/data"
 	"github.com/Maksim-Gr/goBeyond/internal/validator"
 	"net/http"
-	"time"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -55,16 +55,18 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
 	if err != nil {
-		http.NotFound(w, r)
+		app.notFoundResponse(w, r)
 		return
 	}
-	movie := data.Movie{
-		ID:        id,
-		CreatedAt: time.Now(),
-		Title:     "The die another day",
-		Runtime:   102,
-		Genres:    []string{"horror", "action"},
-		Version:   1,
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
 	}
 
 	err = app.WriteJson(w, http.StatusOK, envelope{"movie": movie}, nil)
