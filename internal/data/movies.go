f5d1cebb04988d4fce9c4d0828f6abe7e1b0f43a
Makism-Gr <maksim.g.grishchenko@gmail.com>
2024-02-23 20:47:07 +0300

    update: update Movies controller

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -90,7 +90,16 @@ func (m MovieModel) Update(movie *Movie) error {
 		pq.Array(movie.Genres),
 		movie.ID,
 	}
-	return m.DB.QueryRow(query, args...).Scan(&movie.Version)
+	err := m.DB.QueryRow(query, args...).Scan(&movie.Version)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrEditConflict
+		default:
+			return err
+		}
+	}
+	return nil
 }
 
 func (m MovieModel) Delete(id int64) error {
