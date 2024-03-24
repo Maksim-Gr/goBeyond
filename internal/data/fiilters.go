d1a2cd212a51b1996ffcd9f32fa514afb7e46dad
Makism-Gr <maksim.g.grishchenko@gmail.com>
2024-03-24 20:43:17 +0300

    chore: add sort filters

--- internal/data/fiilters.go
+++ internal/data/fiilters.go
@@ -1,6 +1,9 @@
 package data
 
-import "github.com/Maksim-Gr/goBeyond/internal/validator"
+import (
+	"github.com/Maksim-Gr/goBeyond/internal/validator"
+	"strings"
+)
 
 type Filters struct {
 	Page         int
@@ -16,4 +19,20 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
 	// Check that the sort parameter matches a value in the safelist.
 	v.Check(validator.In(f.Sort, f.SortSafelist...), "sort", "invalid sort value")
+}
+
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafelist {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+	return "ASC"
 }
