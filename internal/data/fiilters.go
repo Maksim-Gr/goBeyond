af3b4a7d4355fc79375972f8e679bf03f7c29aea
Makism-Gr <maksim.g.grishchenko@gmail.com>
2024-03-25 20:49:21 +0300

    chore: add metadata filters

--- internal/data/fiilters.go
+++ internal/data/fiilters.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"github.com/Maksim-Gr/goBeyond/internal/validator"
+	"math"
 	"strings"
 )
 
@@ -12,6 +13,27 @@ type Filters struct {
 	SortSafelist []string
 }
 
+type Metadata struct {
+	CurrentPage  int `json:"current_page,omitempty"`
+	PageSize     int `json:"page_size,omitempty"`
+	FirstPage    int `json:"first_page,omitempty"`
+	LastPage     int `json:"last_page,omitempty"`
+	TotalRecords int `json:"total_records,omitempty"`
+}
+
+func calculateMetadata(totalRecords, page, pageSize int) Metadata {
+	if totalRecords == 0 {
+		return Metadata{}
+	}
+
+	return Metadata{
+		CurrentPage:  page,
+		PageSize:     pageSize,
+		FirstPage:    1,
+		LastPage:     int(math.Ceil(float64(totalRecords) / float64(pageSize))),
+		TotalRecords: totalRecords,
+	}
+}
 func ValidateFilters(v *validator.Validator, f Filters) {
 	// Check that the page and page_size parameters contain sensible values. v.Check(f.Page > 0, "page", "must be greater than zero")
 	v.Check(f.Page <= 10_000_000, "page", "must be a maximum of 10 million")
