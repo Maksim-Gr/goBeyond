27ac22ed856469ecd3c632fd157f4777be859fc7
Makism-Gr <maksim.g.grishchenko@gmail.com>
2024-04-05 20:50:36 +0300

    chore: add migrations for user && matches method

---
+++ internal/data/users.go
@@ -0,0 +1,35 @@
+package data
+
+import (
+	"errors"
+	"golang.org/x/crypto/bcrypt"
+	"time"
+)
+
+type User struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  password  `json:"-"`
+	Activated bool      `json:"activated"`
+	Version   int       `json:"-"`
+}
+
+type password struct {
+	plaintext *string
+	hash      []byte
+}
+
+func (p *password) Matches(plaintextPassword string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword(p.hash, []byte(plaintextPassword))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default:
+			return false, err
+		}
+	}
+	return true, nil
+}
