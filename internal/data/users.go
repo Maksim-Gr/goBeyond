7fd40dea17ac22d6cc0fb7eb33082d06bc9c29a8
Makism-Gr <maksim.g.grishchenko@gmail.com>
2024-04-05 23:23:04 +0300

    chore: validate plaintext password method

--- internal/data/users.go
+++ internal/data/users.go
@@ -35,8 +35,14 @@ func (p *password) Matches(plaintextPassword string) (bool, error) {
 	return true, nil
 }
 
-func ValidatePassword(v *validator.Validator, email string) {
+func ValidateEmail(v *validator.Validator, email string) {
 	v.Check(email != "", "email", "email must be provided")
 	v.Check(validator.Matches(email, validator.EmailRX), "email", "must be a valid email address")
 
+}
+
+func ValidatePasswordPlaintext(v *validator.Validator, password string) {
+	v.Check(password != "", "password", "must be provided")
+	v.Check(len(password) >= 8, "password", "must be at least 8 bytes long")
+	v.Check(len(password) <= 72, "password", "must not be more than 72 bytes long")
 }
