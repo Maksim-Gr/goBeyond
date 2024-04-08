d12696d03d6eb08a36fd7a7a596f85bee73d5f49
Makism-Gr <maksim.g.grishchenko@gmail.com>
2024-04-08 22:39:24 +0300

    chore: complete register handler

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 )
 
-func (app *application) registerHanlder(w http.ResponseWriter, r *http.Request) {
+func (app *application) registerHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
 		Name     string `json:"name"`
 		Email    string `json:"email"`
@@ -36,9 +36,16 @@ func (app *application) registerHanlder(w http.ResponseWriter, r *http.Request) 
 	if err != nil {
 		switch {
 		case errors.Is(err, data.ErrDuplicateEmail):
-			v.AddError()
-
+			v.AddError("email", "a user with this email address already exists")
+			app.failedValidationResponse(w, r, v.Errors)
+		default:
+			app.serverErrorResponse(w, r, err)
 		}
+		return
+	}
+	err = app.WriteJson(w, http.StatusCreated, envelope{"user": user}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
 	}
 
 }
