131cd0c504077a7947e918ff4c28f918ba7da225
Makism-Gr <maksim.g.grishchenko@gmail.com>
2024-04-18 20:45:52 +0300

    chore: add insert method

--- internal/data/tokens.go
+++ internal/data/tokens.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"context"
 	"crypto/rand"
 	"crypto/sha256"
 	"database/sql"
@@ -31,6 +32,17 @@ func (m TokenModel) New(userID int64, ttl time.Duration, scope string) (*Token, 
 		return nil, err
 	}
 	return token, nil
+}
+
+func (m TokenModel) Insert(token *Token) error {
+	query := `INSERT INTO tokens (hash, user_id, expiry, scope)
+				VALUES ($1, $2, $3, $4)`
+	args := []interface{}{token.Hash, token.UserID, token.Expiry, token.Scope}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := m.DB.ExecContext(ctx, query, args...)
 }
 
 func validateTokenPlainText(v *validator.Validator, tokenPlaintext string) {
