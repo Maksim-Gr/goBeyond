3c8f8cdbb22739fb68c03a86003b6a85b89ffbfe
Makism-Gr <maksim.g.grishchenko@gmail.com>
2024-05-11 20:41:44 +0300

    update:  change cors origin logic

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -168,8 +168,18 @@ func (app *application) requirePermission(code string, next http.HandlerFunc) ht
 
 func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Vary", "Origin")
 
+		origin := r.Header.Get("origin")
+
+		if origin != "" && len(app.config.cors.trustedOrigin) != 0 {
+			for i := range app.config.cors.trustedOrigin {
+				if origin == app.config.cors.trustedOrigin[i] {
+					w.Header().Set("Access-Control-Allow-Origin", origin)
+
+				}
+			}
+		}
 		next.ServeHTTP(w, r)
 	})
 }
