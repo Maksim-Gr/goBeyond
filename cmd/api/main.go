20f84ddd88bd9cea4c743587ac3010bc7183dcc3
Makism-Gr <maksim.g.grishchenko@gmail.com>
2024-05-12 20:29:05 +0300

    chore: add version to the exported vars

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"database/sql"
+	"expvar"
 	"flag"
 	"github.com/Maksim-Gr/goBeyond/internal/data"
 	"github.com/Maksim-Gr/goBeyond/internal/jsonlog"
@@ -88,7 +89,7 @@ func main() {
 	defer db.Close()
 
 	logger.PrintInfo("database connection pool established", nil)
-
+	expvar.NewString("version").Set(version)
 	app := &application{
 		config: cfg,
 		logger: logger,
